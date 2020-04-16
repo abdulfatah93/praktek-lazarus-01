d95db2fb2b0bd3393e791a6a8be30fa146530124
unknown <fatah4318@gmail.com>
2020-04-16 19:39:49 +0800

    membuat showmessage Hello world

---
+++ unit1.pas
@@ -0,0 +1,38 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure button(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.button(Sender: TObject);
+begin
+  showmessage('Hello world');
+end;
+
+end.
+
